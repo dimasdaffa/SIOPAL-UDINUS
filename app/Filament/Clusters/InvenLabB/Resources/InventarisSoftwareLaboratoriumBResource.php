442d254a5db85417f9701359177fe5e0c50cdeaf
dimasdaffa <dimas.de2004@gmail.com>
2025-03-05 20:20:18 +0700

    add lab B

---
+++ app/Filament/Clusters/InvenLabB/Resources/InventarisSoftwareLaboratoriumBResource.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Filament\Clusters\InvenLabB\Resources;
+
+use App\Filament\Clusters\InvenLabB;
+use App\Filament\Clusters\InvenLabB\Resources\InventarisSoftwareLaboratoriumBResource\Pages;
+use App\Filament\Clusters\InvenLabB\Resources\InventarisSoftwareLaboratoriumBResource\RelationManagers;
+use App\Models\Inventaris_Software_Laboratorium_B;
+use App\Models\InventarisSoftwareLaboratoriumB;
+use Filament\Forms;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Pages\SubNavigationPosition;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class InventarisSoftwareLaboratoriumBResource extends Resource
+{
+    protected static ?string $model = Inventaris_Software_Laboratorium_B::class;
+
+    protected static ?string $navigationIcon = 'clarity-application-line';
+
+    protected static ?string $slug = 'software-b';
+
+    protected static ?string $navigationLabel = 'Software';
+
+    protected static ?string $modelLabel = 'Inventaris Laboratorium B';
+
+    protected static ?string $cluster = InvenLabB::class;
+
+    protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::Top;
+
+    protected static ?int $navigationSort = 3;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nama')
+                    ->label('Nama Software')
+                    ->required()
+                    ->maxLength(255),
+
+                TextInput::make('versi')
+                    ->label('Versi')
+                    ->required()
+                    ->maxLength(50),
+
+                Textarea::make('keterangan')
+                    ->label('Keterangan')
+                    ->rows(3)
+                    ->maxLength(500),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama')
+                    ->label('Nama Software')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('versi')
+                    ->label('Versi')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('keterangan')
+                    ->label('Keterangan')
+                    ->limit(50),
+            ])
+            ->filters([
+                //
+            ])
+            ->defaultSort('nama', 'asc') // Mengurutkan berdasarkan Nama Software secara alfabetis
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListInventarisSoftwareLaboratoriumBS::route('/'),
+            'create' => Pages\CreateInventarisSoftwareLaboratoriumB::route('/create'),
+            'edit' => Pages\EditInventarisSoftwareLaboratoriumB::route('/{record}/edit'),
+        ];
+    }
+}
